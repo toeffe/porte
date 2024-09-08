1f1126b2adade59770ceb0687d5b80ba28f66f7c
toeffe <magice-daa@hotmail.com>
2024-09-08 21:12:34 +0200

    fghfg

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
         // Your Google Apps Script Web App URL (replace YOUR_DEPLOYED_SCRIPT_URL with the actual URL)
         const apiUrl = 'https://script.google.com/macros/s/AKfycbyyJ-7OOpHOHu3FWiOO1nKJcG5TVS_MSWoasRF8bAWXQHZAt_ftZAiviYmTecKBFun0/exec';
-        const apiUrl2 = 'https://script.google.com/macros/s/AKfycbxdjgJqQFnNVGSoZ8Kxb2j_ijNJ18VKEhGHQELAtDPYsnnd2MqKjr-6cslpkeDhlxyiGw/exec';
 
         function sendData(rowNumber) {
             // Select the row's input fields and dropdowns
@@ -35,29 +34,6 @@
                 console.error('Error:', error);
             });
         }
-
-
-
-        
-fetch(apiUrl2, {
-  method: 'GET',
-  body: JSON.stringify(data),
-  headers: {
-    "Content-Type": "text/plain;charset=utf-8",
-      },
-   })
-.then(response => response.text())
-.then(data => {
-    console.log('Success:', data);
-    alert('Row submitted successfully!');
-})
-.catch((error) => {
-    console.error('Error:', error);
-});
-
-
-
-
 
         function newrow(number) {
           let html = '';
